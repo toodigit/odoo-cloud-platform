5fd7872f3dd3b4e14fbf5be7b0f937de93f7f765
Damien Carlier <damien.carlier@gmail.com>
2025-12-10 16:31:46 +0100

    [MIG] monitoring_status module migration to 19.0

--- monitoring_status/__manifest__.py
+++ monitoring_status/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Monitoring: Status",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "author": "Camptocamp,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "category",
